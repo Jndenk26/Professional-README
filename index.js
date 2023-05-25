1890f0dfade11df9caebaa91e2bff6774858ad47
James Denk <jndenk26@gmail.com>
2023-05-25 15:06:23 -0500

    finalized code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: Include packages needed for this application
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
@@ -55,15 +55,12 @@ const questions = [
   ];
 
   
-
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
       err ? console.error(err) : console.log('README file generated!')
     );
   }
-
-// TODO: Create a function to initialize app
+  
 function init() {
     inquirer.prompt(questions).then((answers) => {
       const readmeContent = generateMarkdown(answers);
